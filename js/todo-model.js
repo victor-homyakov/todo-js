c0668af06c1270962062de189da3dce5a03dcb60
Victor <homyakov@isida.by>
2013-01-22 15:53:21 -0430

    Cr/Ff/IE9-compatible HTML5 drag&drop

--- js/todo-model.js
+++ js/todo-model.js
@@ -1,5 +1,14 @@
+/**
+ * Карточка задачи.
+ */
 var Ticket = Class.create({
-  template: new Template('<div class="#{type}"><span>#{id}</span> <span>#{name}</span></div>'),
+  template: new Template('<div class="ticket #{type}" draggable="true"><span>#{id}</span> <span>#{name}</span></div>'),
+
+  /*initialize: function(json) {
+   this.id = json.id;
+   this.name = json.name;
+   this.type = (json.type == "bug") ? "bug" : "task";
+   },*/
 
   initialize: function(id, name, type) {
     this.id = id;
@@ -16,6 +25,10 @@ var Ticket = Class.create({
   }
 });
 
+Ticket.fromJSON = function(json) {
+  return new Ticket(json.id, json.name, json.type);
+};
+
 /**
  * Возможны две реализации:
  *
@@ -25,64 +38,139 @@ var Ticket = Class.create({
  * 2. Одна коллекция. Состояние запоминается в дополнительном поле каждого объекта Ticket.
  */
 var Tickets = Class.create({
-  prebind: ["onLoadCreate", "onLoadSuccess", "onLoadComplete"],
+  prebind: ["onLoadCreate", "onLoadSuccess", "onLoadComplete", "onLoadException"],
 
   todo: [],
   inprogress: [],
   done: [],
 
-  initialize: function(container) {
-    this.container = container;
+  initialize: function(containerId) {
+    this.containerId = containerId;
+    if (document.loaded) {
+      this.observe();
+    } else {
+      document.observe("dom:loaded", this.observe.bind(this));
+    }
   },
 
+  observe: function() {
+    //$(container).bind('selectstart', function(){this.dragDrop(); return false;});
+    document.on("selectstart", ".ticket", function(event, element) {
+      event.stop();
+      //console.log("selectstart", event, element);
+      element.dragDrop();
+      //return false;
+    });
+
+    this.onDragStart = this.onDragStart || document.on("dragstart", ".ticket", function(event, element) {
+      //console.log("dragstart", event, element);
+      event.dataTransfer.effectAllowed = "copy";
+      event.dataTransfer.setData("Text", element.innerText || element.textContent);
+    });
+
+    this.onDragOver = this.onDragOver || document.on("dragover", ".tickets", function(event, element) {
+      //console.log("dragover", event, element);
+      event.stop();
+      /*if (event.preventDefault) {
+      //console.log("dragover event.preventDefault()");
+      // allows us to drop
+      event.preventDefault();
+      }*/
+      //element.addClassName("over");
+      //event.dataTransfer.dropEffect = "copy";
+      //return false;
+    });
+
+    this.onDragEnter = this.onDragEnter || document.on("dragenter", ".tickets", function(event, element) {
+      event.stop();
+      event.dataTransfer.dropEffect = "copy";
+      //element.addClassName("over");
+      //return false;
+    });
+
+    //this.onDragLeave = this.onDragLeave || document.on("dragleave", ".tickets", function(event, element) {
+    //element.removeClassName("over");
+    //});
+
+    this.onDrop = this.onDrop || document.on("drop", ".tickets", function(event, element) {
+      event.stop();
+      /*if (event.stopPropagation) {
+       event.stopPropagation();
+       }*/
+      var data = event.dataTransfer.getData("Text");
+      console.log("drop", data, "to", element);
+      //el.parentNode.removeChild(el);
+      //element.removeClassName("over");
+      //return false;
+    });
+  },
+
+  stopObserving: function() {
+    $w("onDragStart onDragOver onDragEnter onDrop").each(function(name) {
+      //console.log(name, this[name]);
+      this[name].stop();
+      this[name] = null;
+    }, this);
+  },
+
+  /**
+   * Загрузка из JSON.
+   *
+   * @param {String} name
+   * @param {Object} options
+   */
   load: function(name, options) {
     options = Object.extend({
       method: "get",
       onCreate: this.onLoadCreate,
       onSuccess: this.onLoadSuccess,
       onComplete: this.onLoadComplete,
-      onException: function(request, exception) {
-        console.log("Exception in load()", exception.message, exception);
-      }
+      onException: this.onLoadException
     }, options || {});
     new Ajax.Request(name, options);
   },
 
   onLoadCreate: function() {
-    $(this.container).addClassName("loading");
+    $(this.containerId).addClassName("loading");
   },
 
   onLoadSuccess: function(response) {
     //console.log(response.responseJSON);
-    this.fromJSON(response.responseJSON);
+    this.loadFromJSON(response.responseJSON);
   },
 
   onLoadComplete: function(response) {
-    $(this.container).removeClassName("loading");
+    $(this.containerId).removeClassName("loading");
   },
 
-  fromJSON: function(json) {
-    this.todo = json.todo || [];
-    this.inprogress = json.inprogress || [];
-    this.done = json.done || [];
+  onLoadException: function(request, exception) {
+    console.log("Exception in load()", exception.message, exception);
+  },
+
+  loadFromJSON: function(json) {
+    // можно создавать новые полноценные объекты Ticket,
+    // а можно просто сменить прототип, т.к. поля полностью совпадают
+    //ticket.__proto__ = Ticket.prototype;
+    this.todo = json.todo.map(Ticket.fromJSON);
+    this.inprogress = json.inprogress.map(Ticket.fromJSON);
+    this.done = json.done.map(Ticket.fromJSON);
     this.render();
   },
 
+  appendTicket: function(stateContainer, ticket) {
+    //console.log(ticket);
+    stateContainer.insert({
+      bottom: ticket
+    });
+  },
+
   render: function() {
-    var container = $(this.container);
-    ["todo", "inprogress", "done"].each(function(type) {
-      var typeContainer = container.down("." + type);
-      //console.log(type, typeContainer);
-      typeContainer.update();
-      this[type].each(function(ticket) {
-        // можно создавать новые полноценные объекты Ticket в fromJSON()
-        // а можно просто сменить прототип, т.к. поля полностью совпадают
-        ticket.__proto__ = Ticket.prototype;
-        //console.log(ticket);
-        typeContainer.insert({
-          bottom: ticket
-        });
-      });
+    var container = $(this.containerId);
+    ["todo", "inprogress", "done"].each(function(state) {
+      var stateContainer = container.down("." + state);
+      //console.log(state, stateContainer);
+      stateContainer.update();
+      this[state].each(this.appendTicket.curry(stateContainer), this);
     }, this);
   }
 });
